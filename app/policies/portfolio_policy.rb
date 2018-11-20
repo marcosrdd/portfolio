e1e168629b328c24805c19a266a3353abb8657c7
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-11-20 11:25:11 -0200

    create portfolio

--- app/policies/portfolio_policy.rb
+++ app/policies/portfolio_policy.rb
@@ -2,7 +2,20 @@ class PortfolioPolicy < ApplicationPolicy
   def index?
     true
   end
-
+ 
+ 
+  def create?
+    record.user == user
+  end
+ 
+ 
+  def edit?
+    record.user == user
+  end
+ 
+ 
+  private
+ 
   class Scope < Scope
     def resolve
       @user.portfolios
