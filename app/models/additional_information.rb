aaef85f08b9f1507a79ecb8f49da212bb5b738e8
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-11-22 16:23:17 -0200

    additional information controller

--- app/models/additional_information.rb
+++ app/models/additional_information.rb
@@ -1,4 +1,7 @@
 class AdditionalInformation < ApplicationRecord
   belongs_to :portfolio
   validates :title, :description, presence: true
+
+  belongs_to :block
+  validates :block_id, uniqueness: true
 end
